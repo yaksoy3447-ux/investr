bf64cc4e459c0a0fcf1ac6b1c24781864980f2fa
Yasin AKSOY <outreach@investr.co>
2026-03-30 02:17:02 +0200

    fix(blog): reverse posts array to display newest first

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -37,7 +37,7 @@ export default async function BlogPage({ params }: { params: Promise<{ locale: s
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {posts.map((post) => (
+          {[...posts].reverse().map((post) => (
             <Link 
               href={`/blog/${post.id}`}
               key={post.id} 
